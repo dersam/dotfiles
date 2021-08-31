7384e5a8e368d5115c771af3e259f305a6ef9d86
Sam Schmidt <sam.schmidt@shopify.com>
2021-08-31 10:56:02 -0400

    Add graphiql and db aliases

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,11 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+function spindb() {
+    open "mysql://root@$(spin info fqdn):3306"
+}
+
+function spingraphiql() {
+  open "https://app.$(spin info fqdn)/services/internal/shops/1/graphql"
+}
