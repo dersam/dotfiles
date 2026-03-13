eefde67ef8f661b5cb97b752ced8a89bd9c2af60
Sam Schmidt <sam.schmidt@shopify.com>
2026-03-13 13:38:47 -0400

    Add starship.toml configuration and symlink in install script

--- install.sh
+++ install.sh
@@ -31,6 +31,8 @@ esac
 ln -vsfn $DOTFILES_DIRECTORY_NAME/core/configs/.gitconfig ~/.gitconfig
 ln -vsfn $DOTFILES_DIRECTORY_NAME/core/configs/.gitignore_global ~/.gitignore_global
 
+ln -vsfn $DOTFILES_DIRECTORY_NAME/starship.toml ~/.config/starship.toml
+
 # Symlink this repo's .zshrc to ~/.zshrc. Using a symlink ensures that when the repo is
 # updated, the terminal will pick up the new version on reload without having to run
 # install again. This will overwrite any existing .zshrc.
